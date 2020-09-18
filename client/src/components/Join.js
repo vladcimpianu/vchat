8b4b475dc29dd8ef643336ce2c1717e119192009
Vlad Cimpianu <vlad.cimpianu@fortech.ro>
2020-09-18 17:30:31 +0300

    first commit

---
+++ client/src/components/Join.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Join = () => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  return (
+    <div className="join-outer-container">
+      <div className="join-inner-container">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            placeholder="Name"
+            className="join-input"
+            type="text"
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Room"
+            className="join-input mt-20"
+            type="text"
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+        </div>
+        <Link
+          onClick={(event) => (!name || !room ? event.preventDefault() : null)}
+          to={`/chat?name=${name}&room${room}`}
+        >
+          <button className="btn mt-20" type="submit">
+            Sign In
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default Join;
